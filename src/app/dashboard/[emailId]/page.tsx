3832002e70c4b3d75161c6de6e541437ddb31ba5
Vihaan Verma <vverma270705@gmail.com>
2025-03-05 19:22:35 +0530

    fix params await warning

--- src/app/dashboard/[emailId]/page.tsx
+++ src/app/dashboard/[emailId]/page.tsx
@@ -2,7 +2,15 @@ import { notFound } from "next/navigation";
 import EmailDetail from "@/components/EmailDetail";
 import { emails } from "@/lib/data"; // We'll create this to store sample data
 
-export default function EmailPage({ params }: { params: { emailId: string } }) {
+export default async function EmailPage({
+  params: compParams,
+}: {
+  params: Promise<{ emailId: string }>;
+}) {
+  const params = await compParams;
+  if (!params?.emailId) {
+    return notFound();
+  }
   const email = emails.find((email) => email.id === params.emailId);
 
   if (!email) {
