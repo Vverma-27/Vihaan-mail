e5d938a16d0bfbf5a75a91f42110480322ce5be2
Vihaan Verma <vverma270705@gmail.com>
2025-03-07 15:35:50 +0530

    prod configs

--- backend/drizzle.config.ts
+++ backend/drizzle.config.ts
@@ -5,7 +5,10 @@ export default defineConfig({
   dialect: "postgresql",
   schema: "./schema",
   dbCredentials: {
-    url: `postgresql://${config.POSTGRES_USER}:${config.POSTGRES_PASSWORD}@postgres:5432/${config.POSTGRES_DB}`,
+    url:
+      process.env.NODE_ENV === "production"
+        ? `postgresql://${config.POSTGRES_USER}:${config.POSTGRES_PASSWORD}@postgres:5432/${config.POSTGRES_DB}`
+        : `postgresql://${config.POSTGRES_USER}:${config.POSTGRES_PASSWORD}@localhost:5432/${config.POSTGRES_DB}`,
     ssl: process.env.NODE_ENV === "production" ? "require" : undefined,
   },
 });
